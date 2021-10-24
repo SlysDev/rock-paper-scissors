8465c4e5ff6409f7b6629e719a2a00afa6e6e723
Slys <80560591+SlysDev@users.noreply.github.com>
2021-10-24 18:33:42 -0400

    Removed Sandbox configs, added computer + player functionality.

--- main.js
+++ main.js
@@ -8,4 +8,19 @@ let computerSelection = computerPlay();
 
 function player() {
   let playerValue = prompt('Please type "rock", "paper", or "scissors"');
+  return playerValue;
+}
+
+function computerPlay() {
+  let computerValue = Math.ceil(Math.random() * 3);
+  console.log(computerValue);
+}
+computerPlay();
+
+function gameAnalysis() {
+  
+}
+
+function gameRun() {
+
 }
