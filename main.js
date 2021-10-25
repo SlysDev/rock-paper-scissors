a83ed23171103beabe86787be325e255a83297e1
Jeremy <slysdeveloper@gmail.com>
2021-10-24 21:00:24 -0400

    Added basic game functionality, incomplete.

--- main.js
+++ main.js
@@ -14,11 +14,25 @@ function player() {
 function computerPlay() {
   let computerValue = Math.ceil(Math.random() * 3);
   console.log(computerValue);
-}
-computerPlay();
-
-function gameAnalysis() {
+  if (computerValue == 1) {
+    computerValue == 'rock';
+  } else if (computerValue == 2) {
+    computerValue == 'paper';
+  } else {
+    computerValue == 'scissor';
+  }
   
+  return computerValue;
+}
+
+function gameStatus() {
+  if (playerSelection == 'rock' && computerSelection == 'rock') {
+    return 'tie';
+  } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+    return 'loss';
+  } else if (playerSelection == 'rock' && computerSelection == 'scissor') {
+    return 'win';
+  }
 }
 
 function gameRun() {
